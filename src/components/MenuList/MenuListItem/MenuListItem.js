5c3748a3346d4e4d44cc7a37bbb0cb0579284b14
Rostyslav <brv9473@gmail.com>
2023-12-18 14:43:55 +0200

    Added my project

--- src/components/MenuList/MenuListItem/MenuListItem.js
+++ src/components/MenuList/MenuListItem/MenuListItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import './MenuListItem.css'
 import { MdKeyboardArrowDown } from "react-icons/md";
 import MenuListDropDown from "../MenuListDropDown/MenuListDropDown";
